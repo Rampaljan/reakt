1528ce7cb1fe0976cd9d0eddd441124bb09a4fa6
Rampaljan <rampaljan2008@gmail.com>
2025-03-11 22:12:39 +0100

    trying to get it to work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Ensure this import is from 'react-dom/client'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
